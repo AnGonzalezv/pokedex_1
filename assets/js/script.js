435826b194f591554ee9820051166a091f5ce6f4
ephiet <mphernan@uc.cl>
2021-01-24 20:38:14 -0300

    pokemon al cargar y busqueda por numero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,187 @@
-
+window.onload = pkmRandom();
 document.querySelector("#boton").addEventListener("click", function(){
     traerDatos();
 });
 
 function traerDatos(){
-    // console.log("dentro de la función");
+    //toma el dato ingresado para entregarselo a API
+    let nombrePkm = document.getElementById("nombrePokemon").value;
 
-    let valor = (document.getElementById("busqueda").value).toLowerCase();
+     //si se ingresa en mayus, se pasa a minus para validacion de API
+     if (nombrePkm === nombrePkm.toUpperCase()){
+        nombrePkm = nombrePkm.toLowerCase()
+    }
+    //consultar API con pokemon ingresado anteriormente
+    var request = $.ajax({
+        url: `https://pokeapi.co/api/v2/pokemon/${nombrePkm}`,
+        method: "GET"
+    });
 
-     url=`https://pokeapi.co/api/v2/pokemon/${valor}`;
-
-
-    const api = new XMLHttpRequest();
-
-    api.open("GET", url, true);
-
-    api.send();
-
-     api.onreadystatechange = function(){
-       if(this.readyState == 4 && this.status == 200){
-            var datos = JSON.parse(this.responseText);
-
-            //  var habilidades = [];
-            //  for (ind of datos.abilities){
-            //      habilidades.push(ind.ability.name);
-            //  }
-
-            //  var potencia = [];
-            //  for (ind of datos.held_items){
-            //      potencia.push(ind.item.name);
-            //  }
-           
-            //  var movimientos = [];        
-            // for (ind of datos.moves){
-            //     movimientos.push(ind.move.name);
-            // } 
-             
-             var imagen = datos.sprites.front_shiny
-            //  console.log(imagen);
-            
-            let respImagen = document.querySelector("#respImagen");
-            respImagen.innerHTML = "";
-            respImagen.innerHTML += `<img src="${imagen}">`
-
-            let respHab = document.querySelector("#respHab");
-            respHab.innerHTML="";
-            for (ind of datos.abilities){
-                respHab.innerHTML += `
-                    <tr>
-                    <td>${ind.ability.name}</td>
-                    </tr>
-                `
-            }
-
-            let respPod = document.querySelector("#respPod");
-            respPod.innerHTML="";
-            for (ind of datos.held_items){
-                respPod.innerHTML += `
-                <tr>
-                <td>${ind.item.name}</td>
-                </tr>
-            `
-            }
-
-            let respMov = document.querySelector("#respMov");
-            respMov.innerHTML = "";
-            for (ind of datos.moves){
-                respMov.innerHTML += `
-                    <tr>
-                    <td>${ind.move.name}</td>
-                    </tr>
-                    `
-            }
+    request.done(function( data ) {
+        const POKEMON = {
+            nombre: data.name,
+            id: data.id,
+            tipo: traducirTipo(data.types[0].type.name),
+            imagen: data.sprites.other.dream_world.front_default,
+            altura: (data.height)/10,
+            peso: (data.weight)/10,
+            hp: data.stats[0].base_stat,
+            ataque: data.stats[1].base_stat,
+            defensa: data.stats[2].base_stat,
+            ataqueEspecial: data.stats[3].base_stat,
+            defensaEspecial: data.stats[4].base_stat,
+            velocidad: data.stats[5].base_stat,
         }
-      }
- }
+        imprimirPokemon(POKEMON);
+        grafico(POKEMON);
+    });
+    request.fail(function( error ) {
+        console.log( 'Error: ' , error );
+    });
+}
+
+//pokemon aleatorio al cargar pagina
+function pkmRandom(){
+    pokeNumero= Math.floor(Math.random() * (810 - 1)) + 1;
+    var request = $.ajax({
+        url: `https://pokeapi.co/api/v2/pokemon/${pokeNumero}`,
+        method: "GET"
+    });
+    request.done(function( data ) {
+        //guardar la informacion en un objeto con los datos que utilizaremos
+        const POKEMON = {
+            nombre: data.name,
+            id: data.id,
+            tipo: traducirTipo(data.types[0].type.name),
+            imagen: data.sprites.other.dream_world.front_default,
+            altura: (data.height)/10,
+            peso: (data.weight)/10,
+            hp: data.stats[0].base_stat,
+            ataque: data.stats[1].base_stat,
+            defensa: data.stats[2].base_stat,
+            ataqueEspecial: data.stats[3].base_stat,
+            defensaEspecial: data.stats[4].base_stat,
+            velocidad: data.stats[5].base_stat,
+        }
+        imprimirPokemon(POKEMON);
+        grafico(POKEMON);
+
+    });
+    request.fail(function( error ) {
+        console.log( 'Error: ' , error ); 
+    });
+}
+
+function traducirTipo(tipoPokemon){
+    var tipo
+    switch(tipoPokemon){
+        case 'fire':
+            tipo = 'Fuego';
+            return tipo;
+            break;
+        case 'water':
+            tipo = 'Agua';
+            return tipo;
+            break;
+        case 'plant':
+            tipo = 'Planta';
+            return tipo;
+            break;
+        case 'normal':
+            tipo = 'Normal';
+            return tipo;
+            break;
+        case 'flying':
+            tipo = 'Volador';
+            return tipo;
+            break;
+        case 'bug':
+            tipo = 'Bicho'
+            return tipo;
+            break;
+        case 'fighting':
+            tipo = 'Luchador';
+            return tipo;
+            break;
+        case 'electric':
+            tipo = 'Eléctrico'
+            return tipo;
+            break;
+        case 'poison':
+            tipo = 'Veneno'
+            return tipo;
+            break;
+        case 'psychic':
+            tipo = 'Psíquico'
+            return tipo;
+            break;
+        case 'rock':
+            tipo = 'Roca';
+            return tipo;
+            break;
+        case 'ghost':
+            tipo = 'Fantasma';
+            return tipo;
+            break;
+        case 'ground':
+            tipo = 'Tierra';
+            return tipo;
+            break;
+        case 'fairy':
+            tipo = 'Hada';
+            return tipo;
+            break;
+        case 'dragon':
+            tipo = 'Dragón';
+            return tipo;
+            break;
+        case 'ice':
+            tipo = 'Hielo';
+            return tipo;
+            break;
+        default:
+            tipo = "Desconocido";
+            return tipo;
+            break;
+    }
+}
+
+function imprimirPokemon(pokemon){
+    var imprimirNombre = document.getElementById("nombreDePokemon");
+    var imprimirImagen = document.getElementById("imagenPokemon");
+    var imprimirNumero = document.getElementById("numeroPokedex");
+    var imprimirTipo = document.getElementById("tipo");
+    var imprimirAltura = document.getElementById("altura");
+    var imprimirPeso = document.getElementById("peso");
+    imprimirNombre.innerHTML = `${pokemon.nombre.toUpperCase()}`;
+    imprimirImagen.src = `${pokemon.imagen}`;
+    imprimirNumero.innerHTML = `${pokemon.id}`;
+    imprimirTipo.innerHTML = `${pokemon.tipo}`;
+    imprimirAltura.innerHTML = `${pokemon.altura} m.`;
+    imprimirPeso.innerHTML = `${pokemon.peso} Kg.`;
+}
+ function grafico(pokemon) {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        theme: "light2", // "light2", "dark1", "dark2"
+        animationEnabled: true, 	
+        title:{
+            text: "Estadísticas"
+        },
+        data: [
+        {
+            color: "#21209c",
+            type: "column",
+            legendText: "Puntos",
+            dataPoints: [
+                { label: "HP",  y: pokemon.hp  },
+                { label: "Ataque", y: pokemon.ataque },
+                { label: "Defensa", y: pokemon.defensa },
+                { label: "Ataque Especial",  y: pokemon.ataqueEspecial },
+                { label: "Defensa Especial",  y: pokemon.defensaEspecial }
+            ]
+        }
+        ]
+    });
+    chart.render();
+}
